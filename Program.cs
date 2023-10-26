759fb5aa4907b53d4fdb2e25bb86a7636ff2964b
Felipe Tavaresc <felipe.tavares@outlook.com>
2023-10-25 21:41:25 -0300

    exercicios 02, item 8

--- Program.cs
+++ Program.cs
@@ -133,24 +133,42 @@ using System.Globalization;
 //    Console.WriteLine("Fora de intervalor");
 
 //Exercício 07
-string[] coordenadas = Console.ReadLine().Split(' ');
-double x = double.Parse(coordenadas[0], CultureInfo.InvariantCulture);
-double y = double.Parse(coordenadas[1], CultureInfo.InvariantCulture);
+//string[] coordenadas = Console.ReadLine().Split(' ');
+//double x = double.Parse(coordenadas[0], CultureInfo.InvariantCulture);
+//double y = double.Parse(coordenadas[1], CultureInfo.InvariantCulture);
 
-if (x == 0.0 && y == 0.0)
-    Console.WriteLine("Origem");
-else if (x > 0.0 && y > 0.0)
-    Console.WriteLine("Q1");
-else if (x < 0.0 && y > 0.0)
-    Console.WriteLine("Q2");
-else if (x < 0.0 && y < 0.0)
-    Console.WriteLine("Q3");
-else if (x > 0.0 && y < 0.0)
-    Console.WriteLine("Q4");
-else if (x == 0)
-    Console.WriteLine("Eixo X");
-else if (y == 0)
-    Console.WriteLine("Eixo Y");
+//if (x == 0.0 && y == 0.0)
+//    Console.WriteLine("Origem");
+//else if (x > 0.0 && y > 0.0)
+//    Console.WriteLine("Q1");
+//else if (x < 0.0 && y > 0.0)
+//    Console.WriteLine("Q2");
+//else if (x < 0.0 && y < 0.0)
+//    Console.WriteLine("Q3");
+//else if (x > 0.0 && y < 0.0)
+//    Console.WriteLine("Q4");
+//else if (x == 0)
+//    Console.WriteLine("Eixo X");
+//else if (y == 0)
+//    Console.WriteLine("Eixo Y");
+
+
+//Exercício 08
+double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double imposto;
+if (salario <= 2000.0)
+    imposto = 0.0;
+else if (salario <= 3000.0)
+    imposto = (salario - 2000.0) * 0.8;
+else if (salario <= 4500.0)
+    imposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
+else
+    imposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+
+if (imposto == 0)
+    Console.WriteLine("Isento");
+else
+    Console.WriteLine($"R$ {imposto.ToString("F2", CultureInfo.InvariantCulture)}");
 
 
 
