1cb40b6fddc38f943801708dcb0f699271091d71
Felipe Tavaresc <felipe.tavares@outlook.com>
2023-11-01 19:50:35 -0300

    exercício 04, item 06

--- Program.cs
+++ Program.cs
@@ -278,13 +278,22 @@ using System.Globalization;
 //}
 
 //Exercício 05 - fatorial
+//int n = int.Parse(Console.ReadLine());
+
+//int fat = 1;
+//for (int i = 1; i <= n; i++)
+//    fat = fat * i;
+
+//Console.WriteLine(fat);
+
+//Exercício 06
 int n = int.Parse(Console.ReadLine());
-
-int fat = 1;
 for (int i = 1; i <= n; i++)
-    fat = fat * i;
-
-Console.WriteLine(fat);
+{
+    if (n % i == 0)
+        Console.WriteLine(i);
+}
+    
 
 
 
